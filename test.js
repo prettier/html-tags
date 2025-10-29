98319d8dea68ebd830b347a943daa461a24e2d63
fisker <lionkay@gmail.com>
2025-10-29 10:43:23 +0800

    Update tests

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import assert from "node:assert/strict";
 import test from "node:test";
-import htmlTags from "./index.js";
+import { htmlTags, htmlVoidTags } from "./index.js";
 
-test("Main", () => {
+test("Html Tags", () => {
   assert.ok(Array.isArray(htmlTags));
   assert.equal(new Set(htmlTags).size, htmlTags.length);
   // W3C https://raw.githubusercontent.com/w3c/elements-of-html/HEAD/elements.json
@@ -14,5 +14,15 @@ test("Main", () => {
 
   for (const tag of htmlTags) {
     assert.ok(/^(?:[a-z]+|h[123456])$/.test(tag), tag);
+  }
+});
+
+test("HTML Void Tags", () => {
+  assert.ok(Array.isArray(htmlVoidTags));
+  assert.equal(new Set(htmlVoidTags).size, htmlVoidTags.length);
+  assert.ok(htmlVoidTags.includes("img"));
+
+  for (const tag of htmlVoidTags) {
+    assert.ok(/^[a-z]+$/.test(tag), tag);
   }
 });
